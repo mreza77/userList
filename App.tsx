6f85c13cf3950762b5aee50bcc57dfdf99cdf348
mohammad reza <mojaver77@gmail.com>
2022-02-22 22:38:24 +0330

    Fix: fix bottom sheet drag ion android

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Toast from "react-native-toast-message";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import BaseNavigator from "./src/navigation/BaseNavigator";
 import toastConfig from './src/config/toastConfig';
@@ -12,13 +13,15 @@ import store, { persistor } from "./src/redux/store";
 const App = () => {
   return (
     <>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <NavigationContainer>
-            <BaseNavigator />
-          </NavigationContainer>
-        </PersistGate>
-      </Provider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <NavigationContainer>
+              <BaseNavigator />
+            </NavigationContainer>
+          </PersistGate>
+        </Provider>
+      </GestureHandlerRootView>
       <Toast config={toastConfig} />
     </>
   )
